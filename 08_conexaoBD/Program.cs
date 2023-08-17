7b883b4283c5b9f2f486bf164e9daaf5a59c5325
Eduardo_Lira <135743538+Edulira14@users.noreply.github.com>
2023-08-17 14:02:15 -0300

    Add files via upload

---
+++ 08_conexaoBD/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _08_conexaoBD;
+
+Console.WriteLine("Hello, World!");
+//ConexaoSimples conexaoSimples = new ConexaoSimples();
+
+ConexaoFlexivel conexaoFlexivel= new ConexaoFlexivel();
+conexaoFlexivel.executaQuery("SELECT * FROM tarefas;");
+conexaoFlexivel.executaQuery("SELECT FROM WHERE id = 4;");
